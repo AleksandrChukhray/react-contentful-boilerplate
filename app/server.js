46ddf30ab9e9414ccba446be37284c49add046ff
Mike Smart <mike@mikesmart.co.uk>
2015-12-26 00:32:32 +0100

    Add compression

--- app/server.js
+++ app/server.js
@@ -1,10 +1,12 @@
 import express from 'express'
+import compression from 'compression';
 import contentfulApi from './middleware/contentful-api';
 import render from './middleware/render';
 import favicon from 'express-favicon';
 
 const app = express();
 
+app.use(compression());
 app.use(favicon(`${__dirname}/favicon.ico`));
 app.use('/static', express.static(`${__dirname}/../static`));
 app.use('/api', contentfulApi);
